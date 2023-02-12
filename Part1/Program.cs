88303a17d32adf57154845cad0cbec0ab692e095
razbroc <razbroc@gmail.com>
2023-02-12 14:07:33 +0200

    add(part one classes and basic functions)

--- Part1/Program.cs
+++ Part1/Program.cs
@@ -1,6 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Part1;
 
-/*
- * TODO: Write code for part1.
- * Notice: You need to implement the project structure yourself.
- */
+Judge simon = new Judge(1234512, "Simon Kawel", 1.83, 35, true);
+Contestent gaga = new Contestent(121212, "Lady Gaga", 1.72, true, false);
+Contestent christina = new Contestent(132332, "Christina  Aguilera ", 1.68, false, true);
+
+Song song1 = new Song("SpongeBob", "Who lives in a pineapple under the sea???");
+Song song2 = new Song("Mundial WakaWaka", "Tsamina mina, eh, eh\r\nWaka waka, eh, eh\r\nTsamina mina zangalewa, anawa-a-a\r\n");
+
+christina.Sing(song1);
+gaga.Sing(song2);
+simon.ChooseWinner(christina, gaga);
