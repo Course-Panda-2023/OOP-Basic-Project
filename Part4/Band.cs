851e76bc02706200fee735dd76df7323085fea9e
EYTZIP <eyaltzipor5@gmail.com>
2023-02-12 18:26:28 +0200

    Part4

---
+++ Part4/Band.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopFinal
+{
+    internal class Band : MemberInCompetition,ParticipantInCompetition
+    {
+        private List<Singer> bandMember;
+
+        public Band(string name , List<Singer> bandmember) : base(name)
+        {
+            this.bandMember=new List<Singer>(bandmember);
+        }
+        public override string Desccription()
+        {
+            return "we are the " + name ;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public void Sing(Song song)
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine($"{this.name} ( {song.Name} ) : ");
+            foreach(Singer singer in bandMember) {Console.WriteLine($"{singer.getName()} ( {song.Name} ) : {song.Lyrics}"); }
+            Console.WriteLine("-------------------");
+            
+        }
+    }
+}
---
+++ Part4/ParticipantInCompetition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopFinal
+{
+    internal interface ParticipantInCompetition
+    {
+        public void Sing(Song song);
+        public string getName();   
+    }
+}
