13dfb5cc876114a15bc68ec16e9c8517bc024392
liavbatson <batsonliav@gmail.com>
2023-02-12 11:26:11 +0200

    Finished Part 1

---
+++ Part1/Singer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Singer
+{
+    private string mName;
+    public string Name
+    {
+        get { return mName; }
+        set { mName = value; }
+    }
+    private List<Song> mSongsList;
+    public List<Song> SongsList
+    {
+        get { return mSongsList; }
+        set { mSongsList = value; }
+    }
+    public Singer(string mName)
+    {
+        this.mName = mName;
+        this.mSongsList = new List<Song>();
+    }
+    public Singer(string name, List<Song> songsList)
+    {
+        this.mName = name;
+        this.mSongsList = songsList;
+    }
+    public void AddSong(Song song)
+    {
+        this.mSongsList.Add(song);
+    }
+    public void RemoveSong(Song song)
+    {
+        this.mSongsList.Remove(song);
+    }
+}
---
+++ Part1/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Song
+{
+    private string mSongName;
+    public string SongName
+    {
+        get { return mSongName; }
+        set { mSongName = value; }
+    }
+    public Song(string songName)
+    {
+        this.mSongName = songName;
+    }
+}
