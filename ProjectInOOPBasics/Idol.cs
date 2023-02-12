def845a070b70aef1de1aea110f991927613f530
Assaf Hillel <68805670+amazingProj@users.noreply.github.com>
2023-02-12 21:19:02 +0200

    initial commit

---
+++ ProjectInOOPBasics/Idol.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectInOOPBasics
+{
+    internal class Idol : IIdol
+    {
+        List<IIdol> idols = new List<IIdol>();
+        public Idol(IIdol idol, IIdol anotherIdol) {
+            this.idols.Add(idol);
+            this.idols.Add(anotherIdol);
+        }
+        public void Execute()
+        {
+            var iterator = CollectionsMarshal.AsSpan(idols);
+            foreach (var idol in iterator)
+            {
+                idol.Execute();
+            }
+        }
+    }
+}
---
+++ ProjectInOOPBasics/Program.cs
@@ -0,0 +1,7 @@
+﻿using ProjectInOOPBasics;
+
+Idol idol = new Idol(new Singer("Christine"), new Singer("Lazy"));
+
+idol.Execute();
+
+Console.ReadLine();
