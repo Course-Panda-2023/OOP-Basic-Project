84d547f6d0d9f9f831d80df328112045f574ef8d
guyalfasi <guyalfasi@gmail.com>
2023-02-12 11:20:14 +0200

    finished part 1

--- Part1/Program.cs
+++ Part1/Program.cs
@@ -1,5 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("hello");
+    }
+}
 /*
  * TODO: Write code for part1.
  * Notice: You need to implement the project structure yourself.
