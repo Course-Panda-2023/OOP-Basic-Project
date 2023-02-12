92caec493aabd06d82bc14a0dfd3e3f7b805c08a
omri2084 <omri2084@cs.huji.ac.il>
2023-02-12 15:33:42 +0200

    part3

--- Part2/Program.cs
+++ Part2/Program.cs
@@ -1,5 +1,217 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿string song = "Seems like it was yesterday\r\nWhen I saw your face\r\nYou told me how proud you were,\r\nBut I walked away\r\nIf only I knew what I know today\r\nOoh, ooh";
+string song1 = "A RedOne\r\nConvict\r\nGaga\r\n\r\nI've had a little bit too much\r\nAll of the people start to rush\r\n(Start to rush by)\r\n\r\nCaught in a twisted dance\r\nCan't find my drink or man\r\nWhere are my keys, I left my phone";
+Song ChristinaAguileraSong = new Song("Hurt", song);
+Singer ChristinaAguilera = new Singer("Christina", "Aguilera");
 
-/*
- * TODO: Copy all of your code from part1 and continue here.
- */
+string song1 = "A RedOne\r\nConvict\r\nGaga\r\n\r\nI've had a little bit too much\r\nAll of the people start to rush\r\n(Start to rush by)\r\n\r\nCaught in a twisted dance\r\nCan't find my drink or man\r\nWhere are my keys, I left my phone";
+Song LadyGagaSong = new Song("Just Dance", song1);
+Singer LadyGaga = new Singer("Lady", "Gaga");
+Singer ChristinaAguilera = new Singer("Christina", "Aguilera");
+
+string song2 = "i see you on the street and you walk on by\r\nYou make me wanna hang my head down and cry\r\nIf you gave me half a chance you'd see\r\nMy desire burning inside of me\r\nBut you choose to look the other way\r\nI've had to work much harder than this\r\nFor something I want don't try to resist me\r\n";
+Song MadonnaSong = new Song("Open Your Heart", song2);
+Singer Madonna = new Singer("Madonna", "");
+
+string song3 = "Superstar\r\nWhere you from, how's it going?\r\nI know you\r\nGotta clue, what you're doing?\r\nYou can play brand new to all the other chicks out here\r\nBut I know what you are, what you are, Baby";
+Song BritneySpearsSong = new Song("Open Your Heart", song3);
+Singer BritneySpears = new Singer("Britney", "Spears");
+
+string song4 = "You're a song\r\nWritten by the hands of God\r\nDon't get me wrong 'cuz\r\nThis might sound to you a bit odd\r\nBut you own the place\r\nWhere all my thoughts go hiding\r\nAnd right under your clothes\r\nIs where I find them\r\nUnderneath your clothes\r\nThere's an endless story\r\nThere's the man I chose\r\nThere's my territory\r\nAnd all the things I deserve\r\nFor being such a good girl honey";
+Song ShakiraSong = new Song("Underneath Your Clothes", song4);
+Singer Shakira = new Singer("Shakira", "");
+
+string song5 = "Wait, do you see my heart on my sleeve?\r\nIt's been there for days on end and\r\nIt's been waiting for you to open up\r\nJust you baby, come on now\r\nI'm trying to tell you just how\r\nI'd like to hear the words roll out of your mouth finally\r\nSay that it's always been me";
+Song AdeleSong = new Song("Best For Last", song5);
+Singer Adele = new Singer("Adele", "");
+
+string song6 = "i see you on the street and you walk on by\r\nYou make me wanna hang my head down and cry\r\nIf you gave me half a chance you'd see\r\nMy desire burning inside of me\r\nBut you choose to look the other way\r\nI've had to work much harder than this\r\nFor something I want don't try to resist me\r\n";
+Song AvrilLavigneSong = new Song("Open Your Heart", song6);
+Singer AvrilLavigne = new Singer("Avril", "Lavigne");
+
+string song7 = "Uh, yo. Yo.\r\nIt was back in 07 did a couple of tapes.\r\nDid a couple dvd's make a couple mistakes\r\ndidn't know what I was doing but I put on a cape\r\nnow it's which world tour should I go on today?\r\nSee, you told me I would lose but I won.\r\nMight cop a million Jimmy Choo's just for fun.\r\nCause bitches couldn't take whats in me\r\nAustralasia to Sydney\r\nmight run up in Disney\r\nout in la with Lindsey.";
+Song NickiMinajSong = new Song("I'm The Best", song7);
+Singer NickiMinaj = new Singer("Nicki", "Minaj");
+
+string song8 = "Wake up in the morning feeling like P Diddi\r\n(Hey, what up girl?)\r\nGrab my glasses, I'm out the door - I'm gonna hit this city (Lets go)\r\nBefore I leave, brush my teeth with a bottle of Jack\r\nCause when I leave for the night, I ain't coming back\r\nI'm talkin' bout - pedicures on our toes, toes\r\nTrying on all our clothes, clothes\r\nBoys blowing up our phones, phones\r\nDrop-topping, playing our favorite cd's\r\nPulling up to the parties\r\nTrying to get a little bit tipsy";
+Song KeshaSong = new Song("Tik Tok", song8);
+Singer Kesha = new Singer("Kesha", "");
+
+string song9 = "i will not make the same mistakes that you did\r\nI will not let myself 'cause my heart so much misery\r\nI will not break the way you did\r\nYou fell so hard\r\nI've learned the hard way, to never let it get that fa";
+Song KellyClarksonSong = new Song("Because Of You", song9);
+Singer KellyClarkson = new Singer("Kelly", "Clarkson");
+
+string song10 = "You gotta help me out\r\nIt's all a blur last night\r\nWe need a taxi 'cause your hung over and I'm broke";
+Song KatyPerrySong = new Song("Waking up in Vegas", song10);
+Singer KatyPerry = new Singer("Katy", "Perry");
+
+string song11 = "And you got me like, oh\r\nWhat you want from me? (What you want from me?)\r\nAnd I tried to buy your pretty heart, but the price too high\r\nBaby you got me like, oh, mm\r\nYou love when I fall apart (fall apart)\r\nSo you can put me together\r\n";
+Song RihannaSong = new Song("Love On The Brain", song11);
+Singer Rihanna = new Singer("Rihanna", "");
+
+string song12 = "I feel like fallin' in love (fallin' love)\r\nI'm in the mood to fuck somethin' up (tonight, I'm fuckin' somethin' up, baby)\r\nI need some drink in my cup (I need a drink)\r\nHey (pour me a drink)\r\nI'm in the mood to fuck somethin' up (I'm in the mood to fuck somethin' up)\r\n";
+Song BeyonceSong = new Song("CUFF IT", song12);
+Singer Beyonce = new Singer("Beyoncé", "");
+
+string song13 = "You know you love me (yo), I know you care (uh-huh)\r\nJust shout whenever (yo), and I'll be there (uh-huh)\r\nYou are my love (yo), you are my heart (uh-huh)\r\nAnd we will never, ever, ever be apart (yo, uh-huh)";
+Song JustinBieberSong = new Song("Baby", song13);
+Singer JustinBieber = new Singer("Justin", "Bieber");
+
+Judge SimonCowell = new Judge("Simon", " Cowell");
+LadyGaga.addSong(LadyGagaSong);
+ChristinaAguilera.addSong(ChristinaAguileraSong);
+List<Singer> singers1 = new List<Singer>();
+singers1.Add(LadyGaga);
+singers1.Add(ChristinaAguilera);
+SimonCowell.manageCompetition(singers1);
+
+LadyGaga.addSong(ladygagasong);
+ChristinaAguilera.addSong(christinaaguilerasong);
+BritneySpears.addSong(BritneySpearsSong);
+Shakira.addSong(ShakiraSong);
+Adele.addSong(AdeleSong);
+AvrilLavigne.addSong(AvrilLavigneSong);
+NickiMinaj.addSong(NickiMinajSong);
+Kesha.addSong(KeshaSong);
+KellyClarkson.addSong(KellyClarksonSong);
+KatyPerry.addSong(KatyPerrySong);
+Rihanna.addSong(RihannaSong);
+Beyonce.addSong(BeyonceSong);
+JustinBieber.addSong(JustinBieberSong);
+list<singer> singers1 = new list<singer>()
+{
+    LadyGaga,
+    ChristinaAguilera,
+    BritneySpears,
+    Shakira,
+    Adele,
+    AvrilLavigne,
+    NickiMinaj,
+    Kesha,
+    KellyClarkson,
+    KatyPerry,
+    Rihanna,
+    Beyonce,
+    JustinBieber
+};
+//simoncowell.managecompetition(singers1);
+
+
+
+class Person
+{
+    private string firstName;
+    private string lastName;
+    public string FirstName
+    {
+        get
+        {
+            return firstName;
+        }
+        set
+        {
+            this.firstName = value;
+        }
+    }
+    public string LastName
+    {
+        get
+        {
+            return this.lastName;
+        }
+        set
+        {
+            this.lastName = value;
+        }
+    }
+    public override string ToString()
+    {
+        return $"{this.firstName} {this.lastName}";
+    }
+    public Person(string firstName, string lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+class Song
+{
+    private string word;
+    private string name;
+    public Song(string name, string word)
+    {
+        this.word = word;
+        this.name = name;
+    }
+    public override string ToString()
+    {
+        return $"The name of the song is {this.name} And his words are {this.word}";
+    }
+}
+class Singer : Person
+{
+    List<Song> songs;
+    static Random rnd = new Random();//A variable that helps select songs randomly
+    public Singer(string firstName, string lastName) : base(firstName, lastName)
+    {
+        this.songs = new List<Song>();
+    }
+    public void addSong(Song song)
+    {
+        songs.Add(song);
+    }
+    public void removeSong(Song song)
+    {
+        songs.Remove(song);
+    }
+    public Song singSong()
+    {
+        int r = rnd.Next(songs.Count);
+        return songs[r];
+    }
+}
+class Judge : Person
+{
+    static Random rnd = new Random();//A variable that helps select Winner randomly
+    public Judge(string firstName, string lastName) : base(firstName, lastName)
+    {
+    }
+    private Singer ChooseWinner(List<Singer> singers)
+    {
+        int r = rnd.Next(singers.Count);
+        return singers[r];
+    }
+    public void manageCompetition(List<Singer> singers)
+    public Singer manageCompetition(List<Singer> singers)
+    {
+        //this func return the loser in Duel
+        Song song;
+        foreach (Singer singer in singers)
+        {
+            song = singer.singSong();
+            Console.WriteLine($"And now the singer {singer} will sing: ");
+            Console.WriteLine(song);
+        }
+        Singer winner = this.ChooseWinner(singers);
+        Console.WriteLine($"And the winner is {winner}");
+        return winner == singers[0] ? singers[1] : singers[0];
+    }
+    private List<Singer> getCompeting(List<Singer> singers)
+    {
+        int ranNum1 = rnd.Next(singers.Count);
+        int ranNum2 = rnd.Next(singers.Count);
+        while (ranNum2 == ranNum1)
+            ranNum2 = rnd.Next(singers.Count);
+        return new List<Singer> { singers[ranNum1], singers[ranNum2] };
+
+    }
+    public void Competition(List<Singer> singers)
+    {
+        while (singers.Count != 1)
+        {
+            List<Singer> competing = this.getCompeting(singers);
+            Singer looser = this.manageCompetition(competing);
+            singers.Remove(looser);
+        }
+        Console.WriteLine($"The winner of the competition is {singers[0]}");
+    }
+}
--- Part3/Program.cs
+++ Part3/Program.cs
@@ -3,3 +3,244 @@
 /*
  * TODO: Copy all of your code from part2 and continue here.
  */
+//// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Xml.Linq;
+
+string song = "Seems like it was yesterday\r\nWhen I saw your face\r\nYou told me how proud you were,\r\nBut I walked away\r\nIf only I knew what I know today\r\nOoh, ooh";
+string song1 = "A RedOne\r\nConvict\r\nGaga\r\n\r\nI've had a little bit too much\r\nAll of the people start to rush\r\n(Start to rush by)\r\n\r\nCaught in a twisted dance\r\nCan't find my drink or man\r\nWhere are my keys, I left my phone";
+Song ChristinaAguileraSong = new Song("Hurt", song);
+Song LadyGagaSong = new Song("Just Dance", song1);
+Singer LadyGaga = new Singer("Lady", "Gaga");
+Singer ChristinaAguilera = new Singer("Christina", "Aguilera");
+Judge SimonCowell = new Judge("Simon", " Cowell");
+LadyGaga.addSong(LadyGagaSong);
+ChristinaAguilera.addSong(ChristinaAguileraSong);
+string song13 = "You know you love me (yo), I know you care (uh-huh)\r\nJust shout whenever (yo), and I'll be there (uh-huh)\r\nYou are my love (yo), you are my heart (uh-huh)\r\nAnd we will never, ever, ever be apart (yo, uh-huh)";
+Song JustinBieberSong = new Song("Baby", song13);
+Singer JustinBieber = new Singer("Justin", "Bieber");
+JustinBieber.addSong(JustinBieberSong);
+List<Singer> singers1 = new List<Singer>();
+//singers1.Add(LadyGaga);
+//singers1.Add(ChristinaAguilera);
+//singers1.Add(JustinBieber);
+SimonCowell.Competition(singers1);
+List<Voter> voter1 = new List<Voter>
+{
+    new Voter("aa","bb"),
+    new Voter("aa","bb"),
+    new Voter("aa","bb"),
+    new Voter("aa","bb")
+};
+Competition competition1 = new Competition();
+competition1.competitionWithVotes(voter1, singers1, new List<Song> { JustinBieberSong });
+class Competition
+{
+    public Competition()
+    {
+    }
+
+    public void competitionWithjudge(Judge judge, List<Singer> singers)
+    {
+        judge.Competition(singers);
+    }
+
+    private Dictionary<Singer, int> initVoteDict(List<Singer> singers)
+    {
+        Dictionary<Singer, int> voteDict = new Dictionary<Singer, int>();
+        foreach (Singer singer in singers)
+        {
+            voteDict.Add(singer, 0);
+        }
+        return voteDict;
+    }
+    private Dictionary<Singer, int> removeLosser(int max, Dictionary<Singer, int> voteDict)
+    {
+        foreach (KeyValuePair<Singer, int> kvp in voteDict)
+        {
+            if (kvp.Value < max)
+            {
+                voteDict.Remove(kvp.Key);
+            }
+        }
+        return voteDict;
+    }
+    private int maxVoted(Dictionary<Singer, int> voteDict)
+    {
+        int max = -1;
+        foreach (KeyValuePair<Singer, int> kvp in voteDict)
+        {
+            if (kvp.Value > max)
+            {
+                max = kvp.Value;
+            }
+        }
+        return max;
+    }
+    private List<Singer> dictTolist(Dictionary<Singer, int> voteDict)
+    {
+        List<Singer> singers = new List<Singer>();
+        foreach (KeyValuePair<Singer, int> kvp in voteDict)
+        {
+            singers.Add(kvp.Key);
+        }
+        return singers;
+    }
+    public void competitionWithVotes(List<Voter> voters, List<Singer> singers, List<Song> songs)
+    {
+        Song song;
+        Dictionary<Singer, int> voteDict = this.initVoteDict(singers);
+        foreach (Singer singer in singers)
+        {
+            song = singer.singSong(songs);
+            Console.WriteLine($"And now the singer {singer} will sing: ");
+            Console.WriteLine(song);
+        }
+        foreach (Voter voter in voters)
+        {
+            voteDict[voter.chooseWinner(singers)] += 1;
+        }
+        int max = maxVoted(voteDict);
+        voteDict = this.removeLosser(max, voteDict);
+        if (voteDict.Count == 1)
+        {
+            Console.WriteLine($"the winner is: {voteDict.ElementAt(0).Key}");
+            return;
+        }
+        //in case of tie
+        this.competitionWithVotes(voters, this.dictTolist(voteDict), songs);
+    }
+}
+class Person
+{
+    private string firstName;
+    private string lastName;
+    public string FirstName
+    {
+        get { return firstName; }
+        set { this.firstName = value; }
+    }
+    public string LastName
+    {
+        get { return this.lastName; }
+        set { this.lastName = value; }
+    }
+    public override string ToString()
+    {
+        return $"{this.firstName} {this.lastName}";
+    }
+    public Person(string firstName, string lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+class Voter : Person
+{
+    static Random rnd = new Random();//A variable that helps select winner randomly
+    public Voter(string firstName, string lastName) : base(firstName, lastName) { }
+    public Singer chooseWinner(List<Singer> singers)
+    {
+
+        int r = rnd.Next(singers.Count);
+        return singers[r];
+    }
+}
+class Song
+{
+    private string word;
+    private string name;
+    public Song(string name, string word)
+    {
+        this.word = word;
+        this.name = name;
+    }
+    public override string ToString()
+    {
+        return $"The name of the song is {this.name} And his words are {this.word}";
+    }
+
+}
+class Singer : Person
+{
+    List<Song> songs;
+    static Random rnd = new Random();//A variable that helps select songs randomly
+    public Singer(string firstName, string lastName) : base(firstName, lastName)
+    {
+        this.songs = new List<Song>();
+
+    }
+
+    public void addSong(Song song)
+    {
+        songs.Add(song);
+    }
+
+    public void removeSong(Song song)
+    {
+        songs.Remove(song);
+    }
+
+    public Song singSong()
+    {
+        int r = rnd.Next(songs.Count);
+        return songs[r];
+    }
+    public Song singSong(List<Song> songsArr)
+    {
+        int r = rnd.Next(songsArr.Count);
+        return songs[r];
+    }
+}
+
+class Judge : Person
+{
+    static Random rnd = new Random();//A variable that helps select Winner randomly
+    public Judge(string firstName, string lastName) : base(firstName, lastName)
+    {
+
+    }
+    private Singer ChooseWinner(List<Singer> singers)
+    {
+        int r = rnd.Next(singers.Count);
+        return singers[r];
+    }
+    public Singer manageCompetition(List<Singer> singers)
+    {
+        //this func return the loser in Duel
+        Song song;
+        foreach (Singer singer in singers)
+        {
+            song = singer.singSong();
+            Console.WriteLine($"And now the singer {singer} will sing: ");
+            Console.WriteLine(song);
+        }
+        Singer winner = this.ChooseWinner(singers);
+        Console.WriteLine($"And the winner is {winner}");
+        return winner == singers[0] ? singers[1] : singers[0];
+    }
+    private List<Singer> getCompeting(List<Singer> singers)
+    {
+        int ranNum1 = rnd.Next(singers.Count);
+        int ranNum2 = rnd.Next(singers.Count);
+        while (ranNum2 == ranNum1)
+            ranNum2 = rnd.Next(singers.Count);
+        return new List<Singer> { singers[ranNum1], singers[ranNum2] };
+
+    }
+    public void Competition(List<Singer> singers)
+    {
+        while (singers.Count != 1)
+        {
+            List<Singer> competing = this.getCompeting(singers);
+            Singer looser = this.manageCompetition(competing);
+            singers.Remove(looser);
+        }
+        Console.WriteLine($"The winner of the competition is {singers[0]}");
+    }
+}
+
