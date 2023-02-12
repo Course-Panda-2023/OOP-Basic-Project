4b44b2e775641fe96754c0532ebc2fa6754b13eb
liavbatson <batsonliav@gmail.com>
2023-02-12 17:39:31 +0200

    Finished Part 4

--- Part2/Program.cs
+++ Part2/Program.cs
@@ -1,5 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-/*
- * TODO: Copy all of your code from part1 and continue here.
- */
+﻿Song universalSong1 = new Song("Universal 1");
+Song universalSong2 = new Song("Universal 2");
+List<Song> list = new List<Song>() { universalSong1, universalSong2 };
+Judge judge = new Judge("Liav");
+Singer ladyGaga = new Singer("Lady Gaga", list);
+Singer christinaAguilera = new Singer("Christina Aguilera", list);
+Singer kesha = new Singer("Kesha", list);
+Singer madona = new Singer("Madona", list);
+Singer brittneySpears = new Singer("Brittney Spears", list);
+Singer shakira = new Singer("Shakira", list);
+Singer adele = new Singer("Adele", list);
+Singer rihanna = new Singer("Rihanna", list);
+List<Singer> singers = new List<Singer>() { ladyGaga, christinaAguilera, kesha, madona, brittneySpears, shakira, adele, rihanna };
+Random rnd = new Random();
+while (singers.Count > 1)
+{
+    int firstIndex = rnd.Next(0, singers.Count);
+    int secondIndex = rnd.Next(0, singers.Count);
+    while (firstIndex == secondIndex)
+        secondIndex = rnd.Next(0, singers.Count);
+    Contest contest = new Contest(judge, singers[firstIndex], singers[secondIndex]);
+    Singer eliminate = contest.SimulateRound();
+    singers.Remove(eliminate);
+}
+Console.WriteLine(singers[0].Name + " Wins The Contest!");
---
+++ Part4/ContestDecidedByCrowd.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class ContestDecidedByCrowd
+{
+    private List<ContestParticipant> mParticipantsList;
+    public List<ContestParticipant> SingersList
+    {
+        get { return mParticipantsList; }
+        set { mParticipantsList = value; }
+    }
+    public ContestDecidedByCrowd()
+    {
+        this.mParticipantsList = new List<ContestParticipant>();
+    }
+    public ContestDecidedByCrowd(List<ContestParticipant> list)
+    {
+        this.mParticipantsList = list;
+    }
+    public ContestParticipant SimulateRound()
+    {
+        Random rnd = new Random();
+        if (this.mParticipantsList.Count > 1) 
+        {
+            Dictionary<ContestParticipant, int> dic = new Dictionary<ContestParticipant, int>();
+            foreach (ContestParticipant singer in this.mParticipantsList)
+                dic.Add(singer, 0);
+            Console.WriteLine("Enter Crowd Number");
+            int crowd = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < crowd; i++)
+            {
+                int choice = rnd.Next(0, this.mParticipantsList.Count);
+                ContestParticipant s = dic.ElementAt(choice).Key;
+                dic[s]++;
+            }
+            ContestParticipant loser = dic.Aggregate((l, r) => l.Value < r.Value ? l : r).Key;
+            return loser;
+        }
+        return this.mParticipantsList[0];
+    }
+}
