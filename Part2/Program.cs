34ff3d62b2ff0c7badb1c17e1a64179fc03dcab6
roey2023 <roey.kleks@gmail.com>
2023-02-12 14:25:44 +0200

    finished 2

--- Part2/Program.cs
+++ Part2/Program.cs
@@ -1,5 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-/*
- * TODO: Copy all of your code from part1 and continue here.
- */
+namespace OOPprojectS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song babyBaby = new Song("baby baby", "BABY BABY BABY OHHHHHH");
+            List<Song> songs = new List<Song>();
+            songs.Add(babyBaby);
+            Singer ladyGaga = new Singer("Lady Gaga", songs);
+            Singer christinaAglira = new Singer("Christina Algira", songs);
+            Singer madona = new Singer("Madona", songs);
+            Singer britneySpiers = new Singer("Britney Spiers", songs);
+            Singer shakira = new Singer("Shakira", songs);
+            Singer addel = new Singer("Addel", songs);
+            Singer abrilLabin = new Singer("Abril Labin", songs);
+            Singer nikiMinaj = new Singer("Niki Minaj", songs);
+            Singer kesha = new Singer("Kesha", songs);
+            Singer kellyKlarkston = new Singer("Kelly Klarkston", songs);
+            Singer kateyPerry = new Singer("Katey Perry", songs);
+            Singer rhihanna = new Singer("Rhihanna", songs);
+            Singer beyonce = new Singer("Beyonce", songs);
+            Singer justinBieber = new Singer("Justin Bieber", songs);
+            Judge symonKavel = new Judge("Symon Kavel");
+            List<Singer> singersList = new List<Singer>();
+            singersList.Add(ladyGaga);
+            singersList.Add(christinaAglira);
+            singersList.Add(madona);
+            singersList.Add(britneySpiers);
+            singersList.Add(shakira);
+            singersList.Add(addel);
+            singersList.Add(abrilLabin);
+            singersList.Add(nikiMinaj);
+            singersList.Add(kesha);
+            singersList.Add(kellyKlarkston);
+            singersList.Add(kateyPerry);
+            singersList.Add(rhihanna);
+            singersList.Add(beyonce);
+            singersList.Add(justinBieber);
+            Contest contest = new Contest(singersList, symonKavel);
+            contest.performContest();
+            //symonKavel.contest(ladyGaga, christinaAglira);
+        }
+    }
+}
