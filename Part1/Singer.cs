dd8c042c422fc9984cb04bed67bc1d547c6fd22a
ahmichal <95378632+ahmichal@users.noreply.github.com>
2023-02-12 11:20:23 +0200

    Part1 of the project

---
+++ Part1/Singer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Singer
+{
+    public Singer(string name, string song)
+    {
+        mName = name;
+        mSong = song;
+    }
+    private string mName;
+    private string mSong;
+
+    public string Song
+    {
+        get { return mSong; }
+        set { mName = value; }
+    }
+
+    public string Name
+    {
+        get { return mName; }
+        set { mName = value; }
+    }
+
+
+}
