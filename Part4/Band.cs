82f14e4becb031a0e5d5950c526e2032455b41e4
LeeorBentalIsraeli <leeor2003@gmail.com>
2023-02-12 18:55:02 +0200

    Not final Part4

---
+++ Part4/Band.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+class Band : Performer
+{
+    private string mName;
+    public string Name
+    {
+        get { return mName; }
+        set { mName = value; }
+    }
+
+    private Singer[] mSingers;
+    public Singer[] Singers
+    {
+        get { return mSingers; }
+        set { mSingers = value; }
+    }
+
+    private List<Song> mSongs;
+    public List<Song> Songs
+    {
+        get { return mSongs; }
+        set { mSongs = value; }
+    }
+
+    public Band(string name, Singer[] singers)
+    {
+        mName = name;
+        mSingers = singers;
+        mSongs = new List<Song>();
+    }
+
+    public void AddSong(Song song)
+    {
+        mSongs.Add(song);
+    }
+
+    public void Perform()
+    {
+        Console.WriteLine($"Greatings! We are: {mName} \n");
+        foreach (Song song in mSongs)
+        {
+            Console.WriteLine($"We will now be Performing: {song.Name} \n");
+            foreach (Singer singer in mSingers)
+            {
+                singer.PerformSong(song);
+            }
+        }
+    }
+    
+}
---
+++ Part4/Judge.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Linq;
+
+class Judge
+{
+    private string mName;
+    public string Name
+    {
+        get { return mName; }
+        set { mName = value; }
+    }
+
+    public Judge(string name)
+    {
+        mName= name;
+    }
+
+    public void JudgePerformance()
+    {
+        string[] options = {
+            "I'm sorry, that stinked....",
+            "WOW!!! I don't think i've ever heard something so trash", 
+            "I'd rather step on legos than hear anything come out of your mouth again",
+            "This performance would be amazing, if I were only deaf",
+            "I'm crying... And you can bet it's not tears of joy"
+        };
+        Console.Write($"{mName}: ");
+        Random ran = new Random();
+        int option = ran.Next(0, options.Length - 1);
+        Console.WriteLine(options[option] + "\n");
+
+    }
+    public void ChooseWinner(Singer singer1, Singer singer2)
+    {
+        Random ran = new Random();
+        int winnerIndex = ran.Next(1, 3); //1 for singer 1...
+        Console.WriteLine($"{mName}: This might have been the worst thing I've had to endure, but if I had to pick it would be");
+        switch (winnerIndex)
+        {
+            case 1:
+                Console.WriteLine($"{singer1.Name}!!!!!");
+                break;
+            case 2:
+                Console.WriteLine($"{singer2.Name}!!!!!");
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static void Competition(Singer[] singers)
+    {
+        Random ran = new Random();
+        int newListLength = (int) Math.Ceiling((double) singers.Length/2);
+        if (singers.Length == 1)
+        {
+            Console.WriteLine($"And the overall winner is: {singers[0].Name}!!");
+        }
+        else
+        {
+            Console.WriteLine("This round's winners are: \n");
+            Singer[] newList = new Singer[newListLength];
+            for (int i = 0; i < singers.Length - 1; i += 2)
+            {
+                singers[i].Perform();
+                singers[i+1].Perform();
+                int choice = ran.Next(i, i+2);
+                newList[i/2] = singers[choice];
+                Console.WriteLine($"{singers[i].Name} vs. {singers[i + 1].Name}: {singers[choice].Name}");
+            }
+            if (newListLength != 1)
+            {
+                newList[newListLength - 1] = singers[singers.Length - 1];
+            }
+            Competition(newList);
+        }
+    }
+
+    private static void AudienceVote(Singer[] singers, int audienceNum)
+    {
+        Random ran = new Random();
+        if (singers.Length == 1)
+        {
+            Console.WriteLine($"The winner is: {singers[0].Name}");
+        }
+        else
+        {
+            int[] votes = new int[singers.Length];
+            for (int i = 0; i < audienceNum; i++)
+            {
+                int vote = ran.Next(0, singers.Length);
+                votes[vote]++;
+            }
+            int numTied = 0;
+            int max = votes.Max();
+            for (int i = 0; i < singers.Length; i++)
+            {
+                if (votes[i] == max ) 
+                { 
+                    numTied++;
+                }
+            }
+            Singer[] tied = new Singer[numTied];
+            int j = 0;
+            for (int i = 0; i < singers.Length; i++)
+            {
+                if (votes[i] == max)
+                {
+                    tied[j] = singers[i];
+                    j++;
+                }
+            }
+            AudienceVote(tied, numTied);
+        }
+    }
+    public static void AudienceCompetition(Singer[] singers)
+    {
+        Random ran = new Random();
+        int audienceNum = ran.Next();
+        foreach (Singer singer in singers)
+        {
+            singer.PerformOneOfThree();
+        }
+        AudienceVote(singers, audienceNum);
+
+
+    }
+
+
+}
