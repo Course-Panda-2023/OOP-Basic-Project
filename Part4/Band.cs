7b9b5e89a45247de607af9d4b45a5866e63f3a0f
ahmichal <95378632+ahmichal@users.noreply.github.com>
2023-02-12 18:29:51 +0200

    first commit to part4

---
+++ Part4/Band.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Band : Member
+{
+	private Singer[] mSingers;
+	public Singer[] Singers { get { return mSingers; }}
+	public Band(Singer[] singers, string song)
+	{
+		//mSingers=new Singer[singers.Length];
+		mSingers= singers;
+		foreach(Singer singer in mSingers)
+		{
+			singer.Song = song;
+		}
+	}
+
+    public void Performance()
+    {
+		Console.WriteLine("*together in harmony:*");
+		foreach(Singer singer in mSingers)
+		{
+            Console.WriteLine($"{singer.Name}: {singer.Song}");
+        }
+    }
+}
---
+++ Part4/Member.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public interface Member
+{
+	void Performance();
+
+}
