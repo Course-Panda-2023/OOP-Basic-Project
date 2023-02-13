987cfab8eed4d1a950282ba677f6cae90c022ef4
EYTZIP <eyaltzipor5@gmail.com>
2023-02-13 15:43:34 +0200

    final project commit (part 5)

---
+++ Part5/Drummer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopFinal
+{
+    internal class Drummer : MemberInCompetition, BandMember
+    {
+        public Drummer(string name) : base(name)
+        {
+
+        }
+
+        public override string Desccription()
+        {
+            return "hello my name is " + name + "and i am a drummer";
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public void play(Song song)
+        {
+            Console.WriteLine($"{this.name}-{role()} ( {song.Name} ) : drum noises"); ;
+        }
+
+        public string role()
+        {
+            return "drummer";
+        }
+    }
+}
---
+++ Part5/Singer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopFinal
+{
+    internal class Singer:MemberInCompetition ,ParticipantInCompetition,BandMember
+    {
+
+        public Singer(String singerName):base(singerName) {
+        } 
+
+        public void Sing(Song song) {
+            Console.WriteLine($"{this.name} ( {song.Name} ) : {song.Lyrics}");
+        }
+
+        public override string Desccription()
+        {
+            return "Hello my name is " + this.name + "\nI am a singer in the competition ";    
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public void play(Song song)
+        {
+            Sing(song);
+        }
+
+        public string role()
+        {
+            return "singer";
+        }
+    }
+}
