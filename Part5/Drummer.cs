0197557a20c97e18140eb578f1faa76a92d67165
razbroc <razbroc@gmail.com>
2023-02-13 11:16:29 +0200

    init(initialize guitar and drummer classes)

---
+++ Part5/Drummer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part5
+{
+    public class Drummer : Person
+    {
+        private string DrumsKind;
+        private string SticksMaterial;
+
+        public Drummer(int personId, string name, double highetInMeters, string drumsKind, string sticksMaterial) : base(personId, name, highetInMeters)
+        {
+            this.DrumsKind = drumsKind;
+            this.SticksMaterial = sticksMaterial;
+        }
+    }
+}
---
+++ Part5/Guitarist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part5
+{
+    public class Guitarist : Person
+    {
+        private string GuitarKind;
+
+        public Guitarist(int personId, string name, double highetInMeters, string guitarKind) : base(personId, name, highetInMeters)
+        {
+            this.GuitarKind = guitarKind;
+        }
+    }
+}
