edf50edd3effcead06396b6d90f476a509218715
Fredi Fincheli <fredifin@campus.technion.ac.il>
2023-02-13 22:24:01 +0200

    Adapted previos part for new classes hierarchy Part4 final release

---
+++ Part3/SongCompetition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPBasicProject
+{
+    class SongCompetition
+    {
+        private List<People> _competitionPeople = new List<People>();
+        private People _winner;
+        
+    }
+}
---
+++ Part4/Jury.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPBasicProject
+{    
+    internal class Jury : People
+    {
+        private string _juryName;
+        private int _juryAge;
+        private string _juryCity;
+        private string _juryCountry;
+        private Competitor _singerChoice;
+        //private People _singer;
+
+        public Jury(string name, int age, string city, string country) : base(name, age, city, country)
+        {
+            _juryName = name;
+            _juryAge = age;
+            _juryCity = city;
+            _juryCountry = country;
+            //_singer = new Singer();
+        }
+        public string JuryName
+        {
+            get { return _juryName; }
+            set { _juryName = value; }
+        }
+        public int JuryAge
+        {
+            get { return _juryAge; }
+            set { _juryAge = value; }
+        }
+        public string JuryCity
+        {
+            get { return _juryCity; }
+            set { _juryCity = value; }
+        }
+        public string JuryCountry
+        {
+            get { return _juryCountry; }
+            set { _juryCountry = value; }
+        }
+        
+        public Competitor VoteForSinger(List<Competitor> competitors)
+        {
+            Random rand = new Random();
+            int winnerIndex = rand.Next(competitors.Count);
+            _singerChoice = competitors[winnerIndex];
+            return _singerChoice;
+        }
+        public override string ToString()
+        {
+            return _juryName;
+        }
+        //public 
+    }
+    
+}
