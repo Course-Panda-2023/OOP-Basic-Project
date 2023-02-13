55449148f4b825e8d89c7b7e1568ab62661508f6
liavbatson <batsonliav@gmail.com>
2023-02-13 11:30:42 +0200

    Finished Part 5

---
+++ Part1/Singer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Singer : ContestParticipant
+{
+    private List<Song> mSongsList;
+    public List<Song> SongsList
+    {
+        get { return mSongsList; }
+        set { mSongsList = value; }
+    }
+    public Singer(string name) : base(name)
+    {
+        this.mSongsList = new List<Song>();
+    }
+    public Singer(string name, List<Song> songsList) : base(name)
+    {
+        this.mSongsList = songsList;
+    }
+    public void AddSong(Song song)
+    {
+        this.mSongsList.Add(song);
+    }
+    public void RemoveSong(Song song)
+    {
+        this.mSongsList.Remove(song);
+    }
+}
