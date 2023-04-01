52077df78d22d2bac7ba8e2bc563a299858bc67a
Tamir <tamirk07gmail.com>
2023-04-01 13:42:57 +0300

    OOP-advanced part 1 completed

---
+++ music_project/music_project/ContestantClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contest
+{
+    class Contestant
+    {
+        public string name;
+        public Contestant(string name)
+        {
+            this.name = name;
+        }
+        /// <summary>
+        /// simulates preformance of a song by generating random double between 0 and 1
+        /// </summary>
+        /// <returns></returns>
+        public double sing()
+        {
+            var random = new Random();
+            double song = random.NextDouble();
+            return song;
+        }
+    }
+}
